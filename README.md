525211daa47805bf29d3c49407cf2d7fa8a9b4ad
broadbentt <ghostagent@btinternet.com>
2019-08-28 14:59:53 +0100

    changes

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # BASE64/BLOWFISH ENCODER
-### A SIMPLE PYTHON SCRIPT FILE TO CREATE AND READ BASE64/BLOWFISH ENCODED TEXT STRINGS USING A CORPORATE ENTERPRISE KEY.
+## A SIMPLE PYTHON SCRIPT FILE TO CREATE AND READ BASE64/BLOWFISH ENCODED TEXT STRINGS USING A CORPORATE ENTERPRISE KEY.
 
 Usage: python base64_blowfish.py
 
-| LANGUAGE | FILENAME           | MD5 HASH                         |
-|------    |------              | -------                          |
-| python   | base64_blowfish.py | 6f63162dc5494a1abcbb33709d846f78 |
+| LANGUAGE | FILENAME           | MD5 HASH                         | CONFIDENTIALITY MODE |
+|------    |------              | -------                          | -----                |
+| python   | base64_blowfish.py | f7ca18665c89615e6e1c033e278b6312 | CBC                  |
 
-## CONSOLE DISPLAY
+- [x] For more information on 'confidentiality mode' - see https://csrc.nist.gov/Projects/Block-Cipher-Techniques/BCM
+
+### CIPHER BLOCK CHAINING MODE
+__The Cipher Block Chaining (CBC) mode__ is a confidentiality mode whose encryption process features the combining (“chaining”) of the plaintext blocks with the previous ciphertext blocks. The CBC mode requires an IV to combine with the first plaintext block.  The IV need not be secret, but it must be unpredictable; the generation of such IVs is discussed in Appendix C. Also, the integrity of the IV should be protected.
+
+### PBKDF2
+In cryptology, __Password-Based Key Derivation Function (PBKDF1 and PBKDF2)__ are key derivation functions with a sliding computational cost, used to reduce vulnerabilities to brute force attacks. PBKDF2 is part of RSA Laboratories' Public-Key Cryptography Standards (PKCS) series, specifically PKCS #5 v2.0, also published as Internet Engineering Task Force's RFC 2898. It supersedes PBKDF1, which could only produce derived keys up to 160 bits long. RFC 8018 (PKCS #5 v2.1), published in 2017, recommends PBKDF2 for password hashing.
+
+### CONSOLE DISPLAY
 ![Screenshot](picture1.png)
+
+Approved confidentiality modes include:-
+
+> CBC: An IV-based encryption scheme, the mode is secure as a probabilistic encryption scheme, achieving indistinguishability from random bits, assuming a random IV. 
+> Confidentiality is not achieved if the IV is merely a nonce, nor if it is a nonce enciphered under the same key used by the scheme, as the standard incorrectly suggests to do. 
+> Ciphertexts are highly malleable. 
+> No chosen ciphertext attack (CCA) security. 
+> Confidentiality is forfeit in the presence of a correct-padding oracle for many padding methods.
+> Encryption inefficient from being inherently serial.
+> Widely used, the mode’s privacy-only security properties result in frequent misuse.
+> Can be used as a building block for CBC-MAC algorithms. I can identify no important advantages over CTR mode.
+
+> CFB: Cipher feedback is an IV-based encryption scheme, the mode is secure as a probabilistic encryption scheme, achieving indistinguishability from random bits, assuming a random IV. 
+> However, confidentiality is not achieved if the IV is predictable, nor if it is made by a nonce enciphered under the same key used by the scheme, as the standard incorrectly suggests to do.
+> Ciphertexts are malleable. 
+> No CCA-security. 
+> Encryption inefficient from being inherently serial.
--- base64_blowfish.py
+++ base64_blowfish.py
@@ -16,7 +16,9 @@
 
 import os
 import base64
+from Crypto import Random
 from Crypto.Cipher import Blowfish
+from Crypto.Protocol.KDF import PBKDF2
 
 # -------------------------------------------------------------------------------------
 # AUTHOR  : Terence Broadbent                                                    
@@ -27,13 +29,13 @@ from Crypto.Cipher import Blowfish
 # -------------------------------------------------------------------------------------
 
 os.system("clear")
-print "  __   _  _        __  ____  _     _____        _______ ___ ____  _   _    _____ _   _  ____ ___  ____  _____ ____   " 
-print " / /_ | || |      / / | __ )| |   / _ \ \      / /  ___|_ _/ ___|| | | |  | ____| \ | |/ ___/ _ \|  _ \| ____|  _ \  "
-print "| '_ \| || |_    / /  |  _ \| |  | | | \ \ /\ / /| |_   | |\___ \| |_| |  |  _| |  \| | |  | | | | | | |  _| | |_) | "
-print "| (_) |__   _|  / /   | |_) | |__| |_| |\ V  V / |  _|  | | ___) |  _  |  | |___| |\  | |__| |_| | |_| | |___|  _ <  "
-print " \___/   |_|   /_/    |____/|_____\___/  \_/\_/  |_|   |___|____/|_| |_|  |_____|_| \_|\____\___/|____/|_____|_| \_\ "
-print "                                                                                                                    "
-print "                              BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)                               \n"
+print " ____   __   _  _        __  ____  _     _____        _______ ___ ____  _   _    _____ _   _  ____ ___  ____  _____ ____   " 
+print "| __ ) / /_ | || |      / / | __ )| |   / _ \ \      / /  ___|_ _/ ___|| | | |  | ____| \ | |/ ___/ _ \|  _ \| ____|  _ \  "
+print "|  _ \| '_ \| || |_    / /  |  _ \| |  | | | \ \ /\ / /| |_   | |\___ \| |_| |  |  _| |  \| | |  | | | | | | |  _| | |_) | "
+print "| |_) | (_) |__   _|  / /   | |_) | |__| |_| |\ V  V / |  _|  | | ___) |  _  |  | |___| |\  | |__| |_| | |_| | |___|  _ <  "
+print "|____/ \___/   |_|   /_/    |____/|_____\___/  \_/\_/  |_|   |___|____/|_| |_|  |_____|_| \_|\____\___/|____/|_____|_| \_\ "
+print "                                                                                                                           "
+print "                                  BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)                                  \n"
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
@@ -43,45 +45,57 @@ print "                              BY TERENCE BROADBENT BSC CYBER SECURITY (FI
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
-plainText = "Blessent mon coeur d'une langueur monotone."
-uniqueKey = 'EnterpriseKey'					# min 4 | max 56 bytes
+plainText  = "Blessent mon coeur d'une langueur monotone"
+companyKey = 'W269N-WFGWX-YVC9B-4J6C9-T83GX'
+
+blockSize  = 32 				# 16 to 256 bytes, in multiples of 16 in length.
+padding    = lambda s: s + (blockSize - len(s) % blockSize) * chr(blockSize - len(s) % blockSize)
+unpad      = lambda s: s[:-ord(s[len(s) - 1:])]
 
 # ------------------------------------------------------------------------------------- 
 # AUTHOR  : Terence Broadbent                                                    
 # CONTRACT: GitHub
 # Version : 1.0                                                                
-# Details : Display program data to the screen.
+# Details : Create the functions called from main.
+# Modified: N/A
+# -------------------------------------------------------------------------------------
+
+def Key(companyKey):
+   salt = b'Pots de sel et de poivre'
+   key = PBKDF2(companyKey, salt, 64, 1000)
+   return key[:32]					# Max key size = 56 bytes.
+
+def encrypt(plainText, companyKey):
+   privateKey = Key(companyKey)
+   text = padding(plainText)
+   blocksize = Blowfish.block_size
+   iv = Random.new().read(blocksize)
+   cipher = Blowfish.new(privateKey, Blowfish.MODE_CBC, iv)
+   return base64.b64encode(iv + cipher.encrypt(text))
+
+def decrypt(encryption, companyKey):
+   privateKey = Key(companyKey)
+   decrypted = base64.b64decode(encryption)
+   iv = decrypted[:8]						# Fixed at 8 bytes.
+   cipher = Blowfish.new(privateKey, Blowfish.MODE_CBC, iv)
+   return unpad(cipher.decrypt(decrypted[8:]))   		# Fixed at 8 bytes.
+
+# ------------------------------------------------------------------------------------- 
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub
+# Version : 1.0                                                                
+# Details : MAIN - Display program variables, and encrypt/decrypt plainText.
 # Modified: N/A
 # -------------------------------------------------------------------------------------
 
 print "Plain Text  : " + plainText
-print "Unique Key  : " + uniqueKey
+print "Company Key : " + companyKey
+print "Unique Salt : Pots de sel et de poivre"
+print "Private Key : " + base64.b64encode(Key(companyKey))
+print "Cipher Mode : CBC\n"
 
-bytes = 8								# 8 bytes length
-while len(plainText) % bytes:
-   plainText = plainText + "="         		  # Add appropriate padding to plainText
-
-# ------------------------------------------------------------------------------------- 
-# AUTHOR  : Terence Broadbent                                                    
-# CONTRACT: GitHub
-# Version : 1.0                                                                
-# Details : Encrypt coded message - Base64/Blowfish.
-# Modified: N/A
-# -------------------------------------------------------------------------------------
-
-cipher = Blowfish.new(uniqueKey)
-encrypted = base64.b64encode(cipher.encrypt(plainText))
+encrypted = encrypt(plainText, companyKey)
 print "Encrypted   : " + encrypted
-
-# ------------------------------------------------------------------------------------- 
-# AUTHOR  : Terence Broadbent                                                    
-# CONTRACT: GitHub
-# Version : 1.0                                                                
-# Details : Decrypt coded message - Base64/Blowfish.
-# Modified: N/A
-# -------------------------------------------------------------------------------------
-
-encrypted = base64.b64decode(encrypted)
-print "Decrypted   : " + cipher.decrypt(encrypted).rstrip("=") + "\n"
-
-
+ 
+decrypted = decrypt(encrypted, companyKey)
+print "Decrypted   : " + decrypted + "\n"
